e6ff9132ac09096b1f8254c8567922bce7f58f5a
Sanjeev Midda <s.midda1@gmail.com>
2026-06-29 19:08:15 +0100

    feat(ui): build dashboard layout with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>WEATHER</h1>
+      <h2 id="city">LONDON</h2>
+
+      <div className="cityWeatherDetails">
+        <h4>Temperature: 20 degrees </h4>
+        <h4>Wind Speed: 30mph </h4>
+        <h4>Relative Humidity: 10 degrees </h4>
+        <h4>Weather Code: 341 </h4>
+      </div>
+    </div>
+  );
 }
 
 export default App;
