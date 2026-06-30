6b9c8cb2cb353fe9f83177ad359a098a0c9c3242
Sanjeev Midda <s.midda1@gmail.com>
2026-06-30 22:01:44 +0100

    feat: create utility function to convert weather code into a readable description

---
+++ src/utils/weather.ts
@@ -0,0 +1,15 @@
+// Weather code converter
+export const getWeatherDescription = (code: number): string => {
+  switch (code) {
+    case 0:
+      return "Sunny - get those sunglasses!";
+    case 1:
+      return "Mainly clear - remember to waer sunscreen";
+    case 2:
+      return "Partly cloudly - take a light jacket";
+    case 3:
+      return "Overcast - wrap up warm";
+    default:
+      return "Unknown";
+  }
+};
