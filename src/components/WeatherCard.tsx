67036f584a8b9fb1a129cc9915aa5951077f19cb
Sanjeev Midda <s.midda1@gmail.com>
2026-06-30 20:38:14 +0100

    refactor: remove chaining

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -9,10 +9,10 @@ const WeatherCard = ({ weather }: WeatherCardProps) => {
 
   return (
     <div className="cityWeatherDetails">
-      <h4>Temperature: {weather?.temperature} </h4>
-      <h4>Wind Speed: {weather?.windSpeed} </h4>
-      <h4>Relative Humidity: {weather?.relativeHumidity}</h4>
-      <h4>Weather Code: {weather?.weatherCode} </h4>
+      <h4>Temperature: {weather.temperature} </h4>
+      <h4>Wind Speed: {weather.windSpeed} </h4>
+      <h4>Relative Humidity: {weather.relativeHumidity}</h4>
+      <h4>Weather Code: {weather.weatherCode} </h4>
     </div>
   );
 };
