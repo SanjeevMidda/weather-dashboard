350ecae422d95fdff8160d7f1e06cd9a405fb281
Sanjeev Midda <s.midda1@gmail.com>
2026-06-30 20:56:22 +0100

    render UI based on loading state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import SearchBar from "./components/SearchBar";
 import { useState } from "react";
 import useWeather from "./hooks/useWeather";
 import WeatherCard from "./components/WeatherCard";
+import LoadingState from "./components/LoadingState";
+import ErrorState from "./components/ErrorState";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState<string>("");
@@ -14,7 +16,9 @@ function App() {
       <h1>WEATHER</h1>
       <h2 id="city">{searchQuery}</h2>
 
-      <WeatherCard weather={weather} />
+      {loadingState === "loading" && <LoadingState />}
+      {loadingState === "error" && <ErrorState error={error} />}
+      {loadingState === "successful" && <WeatherCard weather={weather} />}
 
       <SearchBar
         value={searchQuery}
