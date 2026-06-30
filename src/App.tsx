c3717735189a387273d337839e72d1ce752e26b6
Sanjeev Midda <s.midda1@gmail.com>
2026-06-30 19:54:21 +0100

    feat: create useWeather custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,50 @@
 import "./index.css";
 import SearchBar from "./components/SearchBar";
 import { useState } from "react";
-
-type weatherProps = {
-  temperature: number;
-  windSpeed: number;
-  relativeHumidity: number;
-  weatherCode: number;
-};
+import useWeather from "./hooks/useWeather";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState<string>("");
-  const [weather, setWeather] = useState<weatherProps | null>(null);
 
-  const fetchData = async (city: string) => {
-    if (!city.trim()) return;
-    setWeather(null);
+  const { loadingState, weather, error, fetchData } = useWeather();
+  // const fetchData = async (city: string) => {
+  //   if (!city.trim()) return;
+  //   setWeather(null);
 
-    try {
-      const geoRes = await fetch(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
-      );
+  //   try {
+  //     const geoRes = await fetch(
+  //       `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+  //     );
 
-      if (!geoRes.ok) throw new Error("Geocoding failed");
+  //     if (!geoRes.ok) throw new Error("Geocoding failed");
 
-      const geoData = await geoRes.json();
+  //     const geoData = await geoRes.json();
 
-      if (!geoData?.results?.length) {
-        console.log("No city found");
-        return;
-      }
+  //     if (!geoData?.results?.length) {
+  //       console.log("No city found");
+  //       return;
+  //     }
 
-      const { latitude, longitude } = geoData.results[0];
+  //     const { latitude, longitude } = geoData.results[0];
 
-      const weatherRes = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
-      );
+  //     const weatherRes = await fetch(
+  //       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
+  //     );
 
-      if (!weatherRes.ok) throw new Error("Weather fetch failed");
+  //     if (!weatherRes.ok) throw new Error("Weather fetch failed");
 
-      const weatherData = await weatherRes.json();
+  //     const weatherData = await weatherRes.json();
 
-      setWeather({
-        temperature: weatherData.current.temperature_2m,
-        windSpeed: weatherData.current.wind_speed_10m,
-        relativeHumidity: weatherData.current.relative_humidity_2m,
-        weatherCode: weatherData.current.weather_code,
-      });
-    } catch (error) {
-      console.log("Error fetching data", error);
-    }
-  };
+  //     setWeather({
+  //       temperature: weatherData.current.temperature_2m,
+  //       windSpeed: weatherData.current.wind_speed_10m,
+  //       relativeHumidity: weatherData.current.relative_humidity_2m,
+  //       weatherCode: weatherData.current.weather_code,
+  //     });
+  //   } catch (error) {
+  //     console.log("Error fetching data", error);
+  //   }
+  // };
 
   return (
     <div className="App">
---
+++ src/hooks/useWeather.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+//TS loading state options
+type LoadingState = "initial" | "loading" | "error" | "successful";
+
+type Weather = {
+  temperature: number;
+  windSpeed: number;
+  relativeHumidity: number;
+  weatherCode: number;
+};
+
+const useWeather = () => {
+  const [loadingState, setLoadingState] = useState<LoadingState>("initial");
+  const [weather, setWeather] = useState<Weather | null>(null);
+  const [error, setError] = useState<null | string>(null);
+
+  const fetchData = async (city: string) => {
+    if (!city.trim()) return;
+
+    setError(null);
+    setWeather(null);
+    setLoadingState("loading");
+
+    try {
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+      );
+
+      if (!geoRes.ok) throw new Error("Geocoding failed");
+
+      const geoData = await geoRes.json();
+
+      if (!geoData?.results?.length) {
+        setLoadingState("error");
+        setError("City not found");
+        return;
+      }
+
+      const { latitude, longitude } = geoData.results[0];
+
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
+      );
+
+      if (!weatherRes.ok) throw new Error("Weather fetch failed");
+
+      const weatherData = await weatherRes.json();
+
+      setWeather({
+        temperature: weatherData.current.temperature_2m,
+        windSpeed: weatherData.current.wind_speed_10m,
+        relativeHumidity: weatherData.current.relative_humidity_2m,
+        weatherCode: weatherData.current.weather_code,
+      });
+
+      setLoadingState("successful");
+    } catch (error) {
+      setLoadingState("error");
+
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unknown error occurred");
+      }
+    }
+  };
+
+  return { loadingState, error, weather, fetchData };
+};
+
+export default useWeather;
