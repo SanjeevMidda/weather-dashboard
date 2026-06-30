9f5e184bb79ec5dc14d18ac57c3e1fb38d52d84a
Sanjeev Midda <s.midda1@gmail.com>
2026-06-30 20:07:49 +0100

    refactor: remove weather details from main UI

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,6 @@ function App() {
       <h2 id="city">{searchQuery}</h2>
 
       <WeatherCard weather={weather} />
-      <div className="cityWeatherDetails">
-        <h4>Temperature: {weather?.temperature} </h4>
-        <h4>Wind Speed: {weather?.windSpeed} </h4>
-        <h4>Relative Humidity: {weather?.relativeHumidity}</h4>
-        <h4>Weather Code: {weather?.weatherCode} </h4>
-      </div>
 
       <SearchBar
         value={searchQuery}
